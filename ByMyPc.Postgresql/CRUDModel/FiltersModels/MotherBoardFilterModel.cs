62a928fa07c3624a5ccc2289ea9c5fad8113c43c
Dr.Charlatan <sahsasimonenko@gmail.com>
2026-09-05 19:17:00 +0300

    Add to Repo Filtering methods

---
+++ ByMyPc.Postgresql/CRUDModel/FiltersModels/MotherBoardFilterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByMyPc.Postgresql.CRUDModel.FiltersModels
+{
+    internal class MotherBoardFilterModel
+    {
+        public string? ByName { get; set; } = null;
+        public string? ByLive { get; set; } = null;
+        public string? BySocket { get; set; } = null;
+        public string? ByHaveIntegratedGPU { get; set; } = null;
+    }
+}
